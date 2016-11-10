a2fa28d13907ce17a54f5da1302505911925f72e
leeowen <leeowen988@gmail.com>
2016-11-10 08:45:47 +0000

    generalise the sampling.py

--- sampling.py
+++ sampling.py
@@ -1,21 +1,30 @@
 import maya.cmds as cmds
+'''Name the body parts --> sample the contours --> create new contour curves based on the sampling points'''
 	
-for i in range(2):
-	cmds.rename('curve'+str(i+1),'leg_curve'+str(i+1),ignoreShape=False)
+def name_the_part(part):
+	for i in range(2):
+		cmds.rename('curve'+str(i+1),part+'_curve'+str(i+1),ignoreShape=False)
 
-sampleNum=20
-legPoints=[] 
-interval=1.0/sampleNum
-for i in range(2):
-	points=[]
-	for j in range(sampleNum+1):
-		pos=cmds.pointOnCurve('leg_curve'+str(i+1),turnOnPercentage=True,parameter=interval*j) 
-		tuple(pos)
-		points.append(pos)
-	legPoints.append(points)
 
-for i in range(2):
-	curveAfterSample=cmds.curve(point=legPoints[i],degree=3)
-	cmds.rename(curveAfterSample,'leg_curve_afterSampling'+str(i+1))
+def sample_the_contour(sampleNum=20,part='leg',componentPoints):
+	
+	name_the_part(part)
+	
+	interval=1.0/sampleNum
+	for i in range(2):
+		points=[]
+		for j in range(sampleNum+1):
+			pos=cmds.pointOnCurve('leg_curve'+str(i+1),turnOnPercentage=True,parameter=interval*j) 
+			tuple(pos)
+			points.append(pos)
+		componetPoints.append(points)
+
+	#create new contour curves after the sampling
+	for i in range(2):
+		curveAfterSample=cmds.curve(point=componetPoints[i],degree=3)
+		cmds.rename(curveAfterSample,part+'_curve_afterSampling'+str(i+1))
+		
+		
 	
 
+	
