ccc447b1ffb676544ed5af0403e783297edd469e
leeowen <leeowen988@gmail.com>
2017-06-01 15:02:03 +0100

    readin

--- primitive deformer.py
+++ primitive deformer.py
@@ -5,10 +5,25 @@ solve a Euler-Lagrange PDE equation
 
 import scipy as sp
 import numpy as np
+def getpointcount(line):
+    # type: (object) -> object
+    index=line.find(line,0,len(line))
+    count=-1
+    if index!=-1:
+        count = ''.join(x for x in line if x.isdigit())
+        print int(count)
+    return count
 
 def main():
-    #parse argument
-    print"main"
+    #parse argument from .geo file
+    f = open("position.geo","r")
+    next=f.readline()
+    pointcount = 0
+    while next !="":
+        pointcount=getpointcount(next) if getpointcount(next)!=-1 else 0
+        next = f.readline()
+    print pointcount
+    f.close()
 
 
 if __name__ == "__main__":
