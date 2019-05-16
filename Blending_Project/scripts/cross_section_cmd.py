fdd3742638691c3c2dc849bb9df70b6e86f8b8a5
leeowen <leeowen988@gmail.com>
2019-05-16 18:31:57 +0100

    custom cross section maya command

---
+++ Blending_Project/scripts/cross_section_cmd.py
@@ -0,0 +1,23 @@
+# cross_section_cmd.py
+# Author: Ouwen Li
+# Email: david.cuellar@gmail.com
+# Version: 1.0
+# Autodesk Maya Command template API 2.0
+# Copyright (C) 2019 Ouwen Li
+#-------------------------------------------------
+
+import maya.api.OpenMaya as om
+
+
+def maya_useNewAPI():
+    """
+    The presence of this function tells Maya that the plugin produces, and
+    expects to be passed, objects created using the Maya Python API 2.0.
+    """
+    pass
+    
+   
+_pluginName_ = 'cross-sectionCommand'
+_author_ = 'Ouwen Li'
+_version_ = '1.0'
+_apiVersion_ = 'Any'
