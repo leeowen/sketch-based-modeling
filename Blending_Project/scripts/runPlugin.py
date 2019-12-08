fd038da2db1afa278f108e83f707dc871b0ad82f
Ouwen <leeowen988@gmail.com>
2019-12-08 12:02:16 +0800

    scale x,z to 1/300

--- Blending_Project/scripts/runPlugin.py
+++ Blending_Project/scripts/runPlugin.py
@@ -65,7 +65,7 @@ for i in arm:
     cmds.parent( tmp, grp )
     
 
-foreArm=[0.1,0.2,0.3,0.35,0.4,0.5,0.6,0.7,0.8,0.9,1.0]#foreArm=[0.1,0.35,0.5,0.6,0.9,1]
+foreArm=[0.0,0.1,0.2,0.3,0.35,0.4,0.5,0.6,0.7,0.8,0.9,1.0]#foreArm=[0.1,0.35,0.5,0.6,0.9,1]
 for i in foreArm:
     tmp=cmds.crossSectionExtract(mu=i,mmn="source_male_mesh",mbn="Source_RightForeArm",md=40) 
     cmds.parent( tmp, grp )
