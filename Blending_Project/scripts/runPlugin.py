db77abd8008508cee0936b4a15dbbcc456e03b0b
Ouwen <leeowen988@gmail.com>
2019-12-10 08:40:21 +0800

    new leftarm added

--- Blending_Project/scripts/runPlugin.py
+++ Blending_Project/scripts/runPlugin.py
@@ -56,11 +56,13 @@ for i in leg:
     cmds.parent( tmp, grp )
 
     
-arm=[0.32,0.4,0.5,0.55,0.6,0.7,0.8,0.9,0.95,1.0]#arm=[0.32,0.55,0.91,1]
+arm=[0.4,0.5,0.55,0.6,0.7,0.8,0.9,0.95,1.0]#arm=[0.32,0.55,0.91,1]
 for i in arm:
     tmp=cmds.crossSectionExtract(mu=i,mmn="source_male_mesh",mbn="Source_RightArm",md=40) 
     cmds.parent( tmp, grp )
-    
+
+leftArm=[0.16,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95]
+for i in leftArm:
     tmp=cmds.crossSectionExtract(mu=i,mmn="source_male_mesh",mbn="Source_LeftArm",md=40) 
     cmds.parent( tmp, grp )
     
